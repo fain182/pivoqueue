ac229c5f4f5e8071b405f5ded045289b427be927
fain182 <fain182@gmail.com>
2010-10-06 13:38:11 +0200

    show only stories not completed, show stories number

--- pivoqueue.rb
+++ pivoqueue.rb
@@ -9,11 +9,15 @@ PivotalTracker::Client.token = IO.read('./token')
 get '/' do
   projects = PivotalTracker::Project.all
   @stories = {}
+  @count = 0
   projects.each do |project|
     @stories[project.name] = []
     iteration = PivotalTracker::Iteration.current(project)
     iteration.stories.each do |story|
-      @stories[project.name].push story
+      unless story.current_state == "accepted"
+        @stories[project.name].push story
+        @count += 1
+      end
     end
   end
   haml :index
