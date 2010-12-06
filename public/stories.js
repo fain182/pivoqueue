9f52a1d1911667f6d29439096ffa1c52355425fe
fain182 <fain182@gmail.com>
2010-12-06 11:57:46 +0100

    fixed some paths

--- public/stories.js
+++ public/stories.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
 
    function preload_spinner_image() {
         var cacheImage = document.createElement('img');
-        cacheImage.src = 'spinner.gif';
+        cacheImage.src = '/spinner.gif';
     }
 
     //complete stories on click
     $(".done").click(function() {
         $(this).hide();
         $(this).parent().animate({backgroundColor: '#fef7db'}, 'slow');
-        $(this).parent().find(".story_type").css('background-image', 'url(spinner.gif)');
+        $(this).parent().find(".story_type").css('background-image', 'url(/spinner.gif)');
         $.get("done/"+$(this).attr("name"), function (data){
             $('#li_'+data).find(".story_type").remove();
             $('#li_'+data).slideUp('slow', function(){
